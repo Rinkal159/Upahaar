24a4cbf7645c3fe40923b2eda74ac38cb94ff5bb
rinkal159 <rinkalsingapuri@gmail.com>
2025-08-20 18:09:27 +0530

    POST /update/password

---
+++ Backend-Upahaar/Controllers/changePassword.js
@@ -0,0 +1,45 @@
+const User = require("../Model/User");
+const passwordValidation = require("../Utils/passwordValidation");
+const hashPassword = require("../Utils/hashPassword");
+
+const changePassword = async (req, res) => {
+    try {
+        const user = req.user;
+        const userID = req.id;
+
+        const { currentPassword, newPassword, confirmPassword } = req.body;
+
+        if (!currentPassword || !newPassword || !confirmPassword) {
+            throw new Error("All fields are required.")
+        }
+
+        const isCorrectPassword = await user.comparePassword(currentPassword); //function called
+        if (!isCorrectPassword) {
+            throw new Error("Please check your password.")
+        }
+
+        if (newPassword !== confirmPassword) {
+            throw new Error("New password and confirm password do not match.")
+        }
+
+        const errorInPasswordValidation = passwordValidation.validate(newPassword).error; //function called
+        if (errorInPasswordValidation) {
+            throw new Error(errorInPasswordValidation.details[0].message);
+        }
+
+        const hashedPassword = await hashPassword(newPassword); //function called
+
+        await User.updateOne({ _id: userID }, { password: hashedPassword });
+
+        const updatedUser = await User.findById(userID);
+
+        res.json(updatedUser);
+
+    } catch (err) {
+        res.status(400).json({
+            error: err.message
+        })
+    }
+};
+
+module.exports = changePassword;
